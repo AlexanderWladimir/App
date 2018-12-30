9cdcade3b8c070d20bfeb93465752fbd09c51e1f
Alex <bladyalexander333@gmail.com>
2018-12-30 12:00:21 -0500

    enableSwitch

---
+++ App/App/ViewModels/LoginViewModel.cs
@@ -0,0 +1,60 @@
+﻿
+namespace App.ViewModels
+{
+    using GalaSoft.MvvmLight.Command;
+    using System;
+    using System.Windows.Input;
+
+    public class LoginViewModel
+    {
+        #region Properties
+        public string Email
+        {
+            get;
+            set;
+        }
+
+        public string Password
+        {
+            get;
+            set;
+        }
+
+        public bool IsRunning
+        {
+            get;
+            set;
+        }
+
+        public bool IsRemembered
+        {
+            get;
+            set;
+        }
+        #endregion
+
+        #region Constructor
+        public LoginViewModel()
+        {
+            this.IsRemembered = true;
+        }
+        #endregion
+
+        #region Commands
+        public ICommand LoginCommand
+        {
+            get
+                {
+                return new RelayCommand(Login);
+                }
+
+        }
+
+        private void Login()
+        {
+            
+        }
+
+        #endregion
+    }
+}
--- App/App/ViewModels/MainViewModel.cs
+++ App/App/ViewModels/MainViewModel.cs
@@ -3,5 +3,18 @@ namespace App.ViewModels
 {
     class MainViewModel
     {
+        #region ViewModels
+        public LoginViewModel Login {
+            get;
+            set;
+        }
+        #endregion
+
+        #region Constructor
+        public MainViewModel()
+        {
+            this.Login = new LoginViewModel();
+        }
+        #endregion
     }
 }
